115256d4dc9464425bdde7f88650797e3f15fcf4
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-13 17:13:50 +0700

    Menambahkan val buttonApi pada MainActivity.kt dan modifikasi beberapa file

--- app/src/main/java/com/example/apibottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/apibottomsheetdialog/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.apibottomsheetdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonApi: Button = findViewById(R.id.buttonApi)
+        
+        buttonApi.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(
+                this@MainActivity, R.style.BottomSheetDialogTheme
+            )
+
+            val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
+                R.layout.layout_api_bottom_sheet, findViewById<LinearLayout>(R.id.bottomSheet)
+            )
+
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+
+        }
+
     }
 }
